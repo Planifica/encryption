8ff2cf926fbbd62228ab578763f3933398c12981
philippspo <philipp.sporrer@planifica.de>
2015-09-11 23:57:55 +0200

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'planifica:encryption',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Client-Side Encryption for Mongo Collections',
   // URL to the Git repository containing the source code for this package.
