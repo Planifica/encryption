2d5219b14691b88f8bd91014d1b2b8cdec773483
Maxime Quandalle <maxime@quandalle.com>
2015-09-15 03:50:34 +0200

    Remove an unused function

--- utils.js
+++ utils.js
@@ -51,18 +51,6 @@ window.secureShared = {
         return input.replace(/-/g, '+').replace(/_/, '/');
     },
 
-    fileSize: function (fileSizeInBytes) {
-        var i = -1;
-        var byteUnits = [' kB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB',
-            'YB'
-        ];
-        do {
-            fileSizeInBytes = fileSizeInBytes / 1024;
-            i++;
-        } while (fileSizeInBytes > 1024);
-
-        return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
-    },
     uintToString: function(uintArray) {
         var encodedString = String.fromCharCode.apply(null, uintArray),
             decodedString = decodeURIComponent(escape(encodedString));
